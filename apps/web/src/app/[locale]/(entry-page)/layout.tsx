f19dcd7b0b60fcc4a8d6b2e5de2efead3ccac9f6
Kacper Polak <41890821+xStrixU@users.noreply.github.com>
2023-02-05 14:18:15 +0100

    feat: add prettier-plugin-tailwindcss (#10)

--- apps/web/src/app/[locale]/(entry-page)/layout.tsx
+++ apps/web/src/app/[locale]/(entry-page)/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 const EntryPageLayout = ({ children }: { readonly children: ReactNode }) => (
-	<main className="h-full flex items-center justify-center bg-gray-50 dark:bg-neutral-800">
+	<main className="flex h-full items-center justify-center bg-gray-50 dark:bg-neutral-800">
 		{children}
 	</main>
 );
--- apps/web/src/components/common/Inputs/Input/Input.tsx
+++ apps/web/src/components/common/Inputs/Input/Input.tsx
@@ -41,32 +41,32 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 			<div className={sizes[size]['wrapper']}>
 				<label htmlFor={id} className="mb-1 text-neutral-foreground">
 					{label}
-					{required && <span className="text-red-foreground ml-1">*</span>}
+					{required && <span className="ml-1 text-red-foreground">*</span>}
 				</label>
 				<div
 					className={twMerge(
-						'flex items-center relative border border-neutral-stroke-default transition-colors border-b-neutral-stroke-accessible-default hover:border-neutral-stroke-hover hover:border-b-neutral-stroke-accessible-default focus-within:border-neutral-stroke-pressed focus-within:border-b-neutral-stroke-accessible-default bg-neutral-background-default rounded',
-						'after:absolute after:-bottom-px after:-left-px after:-right-px after:h-1 after:border-b-2 after:border-b-brand-default after:rounded-b after:scale-x-0 after:transition-transform after:duration-200 after:clip-path-input-underline focus-within:after:scale-x-100',
+						'relative flex items-center rounded border border-neutral-stroke-default border-b-neutral-stroke-accessible-default bg-neutral-background-default transition-colors focus-within:border-neutral-stroke-pressed focus-within:border-b-neutral-stroke-accessible-default hover:border-neutral-stroke-hover hover:border-b-neutral-stroke-accessible-default',
+						'after:clip-path-input-underline after:absolute after:-bottom-px after:-left-px after:-right-px after:h-1 after:scale-x-0 after:rounded-b after:border-b-2 after:border-b-brand-default after:transition-transform after:duration-200 focus-within:after:scale-x-100',
 						error && 'border-red-border'
 					)}
 				>
-					<div className="flex ml-2 text-neutral-stroke-accessible-default">{contentBefore}</div>
+					<div className="ml-2 flex text-neutral-stroke-accessible-default">{contentBefore}</div>
 					<input
 						ref={ref}
 						id={id}
 						placeholder={label}
 						required={required}
 						className={twMerge(
-							'w-full bg-transparent outline-none text-neutral-foreground',
+							'w-full bg-transparent text-neutral-foreground outline-none',
 							sizes[size]['input'],
 							className
 						)}
 						{...props}
 					/>
-					<div className="flex mr-2 text-neutral-stroke-accessible-default">{contentAfter}</div>
+					<div className="mr-2 flex text-neutral-stroke-accessible-default">{contentAfter}</div>
 				</div>
 				{error && (
-					<p className="text-xs mt-0.5 text-red-foreground flex items-center gap-1">
+					<p className="mt-0.5 flex items-center gap-1 text-xs text-red-foreground">
 						<ErrorIcon />
 						{error.message}
 					</p>
