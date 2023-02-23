34e3e5993c9477c2fe1140b3f032f348afd4319f
Kacper Polak <41890821+xStrixU@users.noreply.github.com>
2023-02-23 11:48:20 +0100

    refactor(web): refactor paths (#37)

--- apps/web/src/components/TeamsListHeader.tsx
+++ apps/web/src/components/TeamsListHeader.tsx
@@ -4,10 +4,16 @@ import { Button } from './ui/Button/Button';
 
 import CreateTeamIcon from '~/icons/create-team.svg';
 
+import { withTeamsPath } from '@/lib/paths';
+
 export const TeamsListHeader = () => (
 	<header className="flex justify-between py-4 px-6">
 		<h1 className="text-xl font-bold text-neutral-foreground">Teams</h1>
-		<Button as={Link} href="/teams/join-or-create" className="flex items-center font-bold">
+		<Button
+			as={Link}
+			href={withTeamsPath('/join-or-create')}
+			className="flex items-center font-bold"
+		>
 			<CreateTeamIcon className="-ml-2" />
 			Join or create team
 		</Button>
