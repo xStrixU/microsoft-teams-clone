b20574d6971fa9856f5ed8115516b14dbf72652a
xStrixU <strixu155@gmail.com>
2023-03-17 20:56:59 +0100

    fix(web): fix build

--- apps/web/src/components/Chat/Chat.tsx
+++ apps/web/src/components/Chat/Chat.tsx
@@ -9,7 +9,7 @@ export const Chat = ({ isNewChat }: ChatProps) => (
 	<div className="flex grow flex-col">
 		{isNewChat && <NewChatHeader />}
 		<div className="mx-auto flex w-full max-w-4xl grow flex-col px-6 py-2">
-			<MessageInput />
+			<MessageInput isNewChat={isNewChat} />
 		</div>
 	</div>
 );
